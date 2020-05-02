f5919e148b6d38becb8045b7a95a9edf1b852424
Szymon Michniak <s.michniak@icloud.com>
2020-05-02 22:28:09 +0200

    Added input parser (not fully working)

---
+++ src/inputParser.c
@@ -0,0 +1,5 @@
+//
+// Created by Szymon Michniak on 02/05/2020.
+//
+
+#include "inputParser.h"
---
+++ src/inputParser.h
@@ -0,0 +1,8 @@
+//
+// Created by Szymon Michniak on 02/05/2020.
+//
+
+#ifndef GAMMA_INPUTPARSER_H
+#define GAMMA_INPUTPARSER_H
+
+#endif //GAMMA_INPUTPARSER_H
